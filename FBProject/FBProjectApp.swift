97b6423f29109e309882bf77984304b88fe980c2
Mohammed Shaat <mohammedshaat.it@gmail.com>
2026-07-22 03:18:03 -0400

    Initial Commit

---
+++ FBProject/FBProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBProjectApp.swift
+//  FBProject
+//
+//  Created by Mohammed on 7/22/26.
+//
+
+import SwiftUI
+
+@main
+struct FBProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
